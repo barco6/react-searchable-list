08a3d54ffbf1f0b33022e2a67e0e48214771ea12
bc <barco@wpsemantix.com>
2020-11-27 18:30:49 +0200

    Added first version of SearchableList

--- example/src/App.js
+++ example/src/App.js
@@ -1,10 +1,43 @@
 import React from 'react'
 
-import { ExampleComponent } from 'react-searchable-list'
+import { SearchableList } from 'react-searchable-list'
 import 'react-searchable-list/dist/index.css'
 
 const App = () => {
-  return <ExampleComponent text="Create React Library Example 😄" />
+  const stringData = ["Lucy", "is", "the", "best", "dog", "ever!", "Mamas", "EMPIRE"];
+  const objectData = [
+    { name: "Lucy", value: "lucy", },
+    { name: "is", value: "is", },
+    { name: "the", value: "the", },
+    { name: "best", value: "best", },
+    { name: "dog", value: "dog", },
+    { name: "ever!", value: "ever!", }
+  ];
+  // return <SearchableList text="Create React Library Example 😄" />
+  return (
+    <div className="App">
+      <div style={{ height: '50%' }}>
+        <SearchableList
+          name="string-list"
+          data={stringData}
+        />
+      </div>
+      {/* <div style={{ height: '30%' }}>
+        <SearchableList
+          name="object-list-one-filterby"
+          data={objectData}
+          filterBy="value"
+        />
+      </div>
+      <div style={{ height: '30%' }}>
+        <SearchableList
+          name="object-list-multi-filterby"
+          data={objectData}
+          filterBy={Object.keys(objectData[0])}
+        />
+      </div> */}
+    </div>
+  );
 }
 
 export default App
--- src/index.js
+++ src/index.js
@@ -1,6 +1,96 @@
-import React from 'react'
-import styles from './styles.module.css'
+import React, { useState, useMemo } from 'react';
+import PropTypes from 'prop-types';
 
-export const ExampleComponent = ({ text }) => {
-  return <div className={styles.test}>Example Component: {text}</div>
+import 'bootstrap/dist/css/bootstrap.min.css';
+import styles from './styles.module.less'
+
+const propTypes = {
+  name: PropTypes.string.isRequired,
+  data: PropTypes.array.isRequired,
+  filterBy: PropTypes.oneOfType([
+    PropTypes.arrayOf(PropTypes.string),
+    PropTypes.string
+  ]),
+  placeholder: PropTypes.string,
+  defaultSearch: PropTypes.string,
+  listStyle: PropTypes.object,
+  listItemStyle: PropTypes.object,
+  listRenderer: PropTypes.func,
+  listItemsRenderer: PropTypes.func,
+};
+
+const defaultProps = {
+  data: [],
+  placeholder: 'Search...',
+  defaultSearch: '',
+  listStyle: {},
+  listItemStyle: {},
+};
+
+
+export const SearchableList = (props) => {
+  const clearString = val => val?.trim().toLowerCase();
+  const [search, changeSearch] = useState(props.defaultSearch);
+
+  const isSearchIncluded = (item) => {
+    if (!(item && search.length)) {
+      return true;
+    }
+
+    const clearSearch = clearString(search);
+    let { filterBy } = props;
+
+    if (typeof item === 'string') {
+      return clearString(item).includes(clearSearch);
+    } else if (filterBy) {
+      if (!Array.isArray(filterBy)) {
+        filterBy = [filterBy];
+      }
+      return filterBy.find(filterKey =>
+        clearString(item[filterKey]).includes(clearSearch));
+    }
+    return JSON.stringify(item).includes(search);
+  };
+
+  const getFilteredList = () => {
+    if (!search.length) {
+      return props.data;
+    }
+    return props.data.filter(isSearchIncluded).filter(fi => !!fi);
+  };
+
+  const filteredList = useMemo(getFilteredList);
+
+  return (
+    <div className={styles.SearchableListContainer}>
+      <div id="search-box">
+        <input
+          type="text"
+          placeholder={props.placeholder}
+          value={search}
+          onChange={newSearch => changeSearch(newSearch.target.value)}
+          disabled={!props.data.length}
+        />
+      </div>
+      {props.listRenderer ? (
+        props.listRenderer(filteredList, search)
+      ) : (
+          <ul style={props.listStyle}>
+            {filteredList.map((item, i) => (
+              <li
+                key={`searchable-${props.name}-item#${i}`}
+                style={{ ...defaultProps.listItemStyle, ...props.listItemStyle }}
+              >
+                {props.valuesRenderer
+                  ? props.valuesRenderer(item)
+                  : item}
+              </li>
+            ))}
+          </ul>
+        )}
+    </div>
+  );
 }
+
+SearchableList.propTypes = propTypes;
+SearchableList.defaultProps = defaultProps;
