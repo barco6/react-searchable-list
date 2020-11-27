aeb1f559c1df70d6f5fe287ddc1cb43085c376b1
bc <barco@wpsemantix.com>
2020-11-27 18:35:41 +0200

    Updates

---
+++ precache-manifest.21ee8dac52b273530af0f56d26b47b7a.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7b7fa0f3fca6d488c636ebf201d0248a",
+    "url": "./index.html"
+  },
+  {
+    "revision": "37a92c84496b534b91b7",
+    "url": "./static/css/2.99a2b36c.chunk.css"
+  },
+  {
+    "revision": "3823a33d6437c71696f7",
+    "url": "./static/css/main.8cf5c0e5.chunk.css"
+  },
+  {
+    "revision": "37a92c84496b534b91b7",
+    "url": "./static/js/2.e3cc6423.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.e3cc6423.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3823a33d6437c71696f7",
+    "url": "./static/js/main.02ecfb24.chunk.js"
+  },
+  {
+    "revision": "6fce53c7c7713ebf61712cc2929746fa",
+    "url": "./static/js/main.02ecfb24.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c2083f3f6ad89fc9892a",
+    "url": "./static/js/runtime-main.895b373d.js"
+  }
+]);
